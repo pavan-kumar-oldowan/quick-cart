9d3c91d11382f547b034486cb6a0062debcd4986
pavankumar <pavankumar.chittiboina@oldowaninnovations.com>
2025-11-05 22:00:48 +0530

    Day 1: Intial setup and basic types

---
+++ src/main.js
@@ -0,0 +1,7 @@
+var itemName = "TV";
+var itemPrice = 5000;
+var isAvailable = true;
+console.log(itemName, itemPrice, isAvailable);
+// create an Array 
+var productsAvailable = ["TV", "FANS", "AC", "MOBILES"];
+console.log(productsAvailable);
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+
+ let itemName:string = "TV";
+
+ let itemPrice:number = 5000;
+
+ let isAvailable:boolean = true;
+
+console.log(itemName,itemPrice,isAvailable);
+
+ // create an Array 
+
+ let productsAvailable :string[] =["TV","FANS","AC","MOBILES"]
+ console.log(productsAvailable)
