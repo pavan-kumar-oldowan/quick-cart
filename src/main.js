311132b1fbf0188c750a90bfe44b60e5ab1ac389
pavankumar <pavankumar.chittiboina@oldowaninnovations.com>
2025-11-05 22:34:43 +0530

    Day 2: Created Product interface and display func on

--- src/main.js
+++ src/main.js
@@ -5,3 +5,18 @@ console.log(itemName, itemPrice, isAvailable);
 // create an Array 
 var productsAvailable = ["TV", "FANS", "AC", "MOBILES"];
 console.log(productsAvailable);
+// create a AllProductArray it Accepts only product [] array only
+var AllProduct = [];
+// create a sample products objects
+var product1 = { id: 1, name: "TV", price: 8000, description: "24-inch display" };
+var product2 = { id: 2, name: "Fans", price: 6000 };
+var product3 = { id: 3, name: "AC", price: 60000, description: "3-star" };
+// add the sproducts in allProduct[]
+AllProduct.push(product1, product2, product3);
+console.log(AllProduct);
+// create a DisplayFunction
+function displayProduct(prod) {
+    console.log("itemName is :".concat(prod.name));
+    console.log("itemPrice is :".concat(prod.price));
+}
+displayProduct(product1);
--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,35 @@ console.log(itemName,itemPrice,isAvailable);
 
  let productsAvailable :string[] =["TV","FANS","AC","MOBILES"]
  console.log(productsAvailable)
+  
+ // create a Interface
+
+ interface product {
+    readonly id:number;
+    name:string;
+    price:number;
+    description?:string;
+ }
+
+ // create a AllProductArray it Accepts only product [] array only
+
+ let AllProduct:product[]=[];
+
+// create a sample products objects
+ let product1: product = { id: 1, name: "TV", price: 8000, description: "24-inch display" };
+ let product2: product = { id: 2, name:"Fans", price: 6000 };
+ let product3: product = { id: 3, name: "AC", price: 60000, description: "3-star" };
+
+ // add the sproducts in allProduct[]
+
+ AllProduct.push(product1,product2,product3)
+ console.log(AllProduct)
+
+ // create a DisplayFunction
+
+ function displayProduct(prod:product):void{
+    console.log(`itemName is :${prod.name}`)
+    console.log(`itemPrice is :${prod.price}`);
+ }
+
+ displayProduct(product1);
